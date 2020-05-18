bcdc4d2403f3e2acb702cfbf343fc09fadf82dbe
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-05-17 22:43:53 -0400

    update

--- full/ansible.sh
+++ full/ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 START_TIME=$SECONDS
-ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i hosts 00-deploy.yml --ask-pass
+ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i hosts site.yml --ask-pass
 echo
 echo "Tempo de execução: $(($SECONDS - $START_TIME))s"
